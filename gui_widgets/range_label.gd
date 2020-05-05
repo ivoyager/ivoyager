8cec464c5c3a423b93ad2b0fbb879dc8d6c047be
Charlie Whitfield <charliewhitfield@zoho.com>
2020-05-05 15:20:41 -0400

    QtyStrings API changes (& RangeLabel fix)

--- gui_widgets/range_label.gd
+++ gui_widgets/range_label.gd
@@ -41,7 +41,7 @@ func _disconnect_camera() -> void:
 	_camera = null
 
 func _on_range_changed(new_range: float) -> void:
-	text = _qty_strings.number_option(new_range, _qty_strings.LENGTH_M_KM_AU)
+	text = _qty_strings.number_option(new_range, _qty_strings.LENGTH_M_KM_AU, "", 3)
 
 func _on_camera_lock_changed(is_locked: bool) -> void:
 	visible = is_locked
