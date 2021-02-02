0d83fbf7666be8d4a16e6f876b0b4a4147eddd01
Charlie Whitfield <charliewhitfield@zoho.com>
2021-02-01 23:02:10 -0500

    Preliminary SimTimer changes

--- prog_refs/sim_timer.gd
+++ prog_refs/sim_timer.gd
@@ -17,21 +17,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # *****************************************************************************
-# [Not added in core ivoyager!] Add to ProjectBuilder.program_references for a
-# timer that uses simulation time rather than engine time.
-#
-# This works very differently than Godot's core Timer class! You will have only
-# one SimTimer. However, any number of objects can obtain and hook up to their
-# own timer signals using make_interval_signal(). Assumes forward time only.
-# Max signal frequency will be once per frame.
+# This works very differently than Godot's core Timer class! There is only one
+# SimTimer. However, any number of objects can obtain and hook up to their
+# own timer signals using make_interval_signal(). Max signal frequency will be
+# once per frame.
+
+# WIP - Make work in reverse time!
+
 
 class_name SimTimer
 
 var _times: Array = Global.times
 var _timekeeper: Timekeeper
-var _ordered_signal_infos := [[INF]] # last element in array is next to signal
+var _allow_time_reversal: bool = Global.allow_time_reversal
+var _forward_signals := [[INF]] # last element in array is next to signal
+
+var _reverse_signals := [[-INF]] # ???? Or reverse above ????
+
 var _recycled_signals := []
 var _counter := 0
+var _is_reversed := false
 
 
 func make_interval_signal(interval: float, one_time := false) -> String:
@@ -50,18 +55,18 @@ func make_interval_signal(interval: float, one_time := false) -> String:
 		add_user_signal(signal_str)
 	var signal_time: float = _times[0] + interval
 	var signal_info := [signal_time, interval, signal_str, one_time]
-	var index := _ordered_signal_infos.bsearch_custom(signal_time, self, "_order")
-	_ordered_signal_infos.insert(index, signal_info)
+	var index := _forward_signals.bsearch_custom(signal_time, self, "_order")
+	_forward_signals.insert(index, signal_info)
 	return signal_str
 
 func recycle_signal(signal_str: String) -> void:
 	# Recycle signal when it's safe for another subscriber to change & use it.
 	assert(!_recycled_signals.has(signal_str), "Signal already recycled")
-	var n_signals := _ordered_signal_infos.size()
+	var n_signals := _forward_signals.size()
 	var index := 1 # skip 0th element which is always [INF]
 	while index < n_signals:
-		if signal_str == _ordered_signal_infos[index][2]:
-			_ordered_signal_infos.remove(index)
+		if signal_str == _forward_signals[index][2]:
+			_forward_signals.remove(index)
 			call_deferred("_append_recycled", signal_str)
 			return
 		index += 1
@@ -70,10 +75,15 @@ func recycle_signal(signal_str: String) -> void:
 func project_init():
 	_timekeeper = Global.program.Timekeeper
 	_timekeeper.connect("processed", self, "_timekeeper_process")
+	_timekeeper.connect("speed_changed", self, "_on_speed_changed")
+
+func _on_speed_changed(_speed_index: int, is_reversed: bool, _is_paused: bool,
+		_show_clock: bool, _show_seconds: bool, _is_real_world_time: bool) -> void:
+	_is_reversed = is_reversed
 
 func _timekeeper_process(sim_time: float, _engine_delta: float) -> void:
-	while sim_time > _ordered_signal_infos[-1][0]: # fast negative result!
-		var signal_info: Array = _ordered_signal_infos.pop_back()
+	while sim_time > _forward_signals[-1][0]: # fast negative result!
+		var signal_info: Array = _forward_signals.pop_back()
 		var signal_str: String = signal_info[2]
 		var one_time: bool = signal_info[3]
 		if one_time:
@@ -86,8 +96,8 @@ func _timekeeper_process(sim_time: float, _engine_delta: float) -> void:
 				signal_time = sim_time # will signal next frame
 			signal_info[0] = signal_time
 			# high frequency will be near end, reducing insert cost
-			var index := _ordered_signal_infos.bsearch_custom(signal_time, self, "_order")
-			_ordered_signal_infos.insert(index, signal_info)
+			var index := _forward_signals.bsearch_custom(signal_time, self, "_order")
+			_forward_signals.insert(index, signal_info)
 		emit_signal(signal_str)
 
 func _append_recycled(signal_str: String) -> void:
