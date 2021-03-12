d56976da44fab7d62d1ca66337ef49cd1830f9d4
Charlie Whitfield <charliewhitfield@zoho.com>
2021-03-12 12:12:53 -0500

    TableImporter/Reader optimizations

--- prog_importers/table_importer.gd
+++ prog_importers/table_importer.gd
@@ -34,6 +34,9 @@
 #  Units (optional; REAL only!). Reals will be converted from provided units
 #    symbol. The symbol must be present in UnitDefs.MULTIPLIERS or FUNCTIONS or
 #    replacement dicts specified in Global.unit_multipliers, .unit_functions.
+#
+# False is represented internally as "", so bool(internal_value) will give
+# meaningful result for both BOOL and X type.
 
 class_name TableImporter
 
@@ -55,8 +58,8 @@ var _table_units := {}
 var _table_row_dicts := {}
 var _table_rows: Dictionary = Global.table_rows # Global shared
 var _wiki_titles: Dictionary = Global.wiki_titles # Global shared
-var _value_indexes := {"" : 0} # preloaded 0-index is null value
-var _values := [""]
+var _unique_register := {}
+
 # localization
 var _enable_wiki: bool = Global.enable_wiki
 var _enums: Script = Global.enums
@@ -71,8 +74,12 @@ var _defaults: Array
 var _line_array: Array
 var _row_name: String
 var _field: String
+var _data_type: String
 var _cell: String
-var _count := 0
+var _count_rows := 0
+var _count_cells := 0
+var _count_non_null := 0
+
 
 func _init():
 	# Monster reg ex found somewhere on internet. Splits comma dilineated,
@@ -81,12 +88,12 @@ func _init():
 	var start_time := OS.get_system_time_msecs()
 	_import()
 	var time := OS.get_system_time_msecs() - start_time
-	print("Imported %s table values (%s unique) in %s msec" % [_count, _values.size(), time])
+	print("Imported tables in %s msec; %s rows; %s cells, %s non-null; %s unique" \
+			% [time, _count_rows, _count_cells, _count_non_null, _unique_register.size()])
 
 func _project_init() -> void:
 	var table_reader: TableReader = Global.program.TableReader
-	table_reader.init_tables(_table_data, _table_fields, _table_data_types, _table_units, 
-			_table_row_dicts, _values)
+	table_reader.init_tables(_table_data, _table_fields, _table_data_types, _table_units, _table_row_dicts)
 	Global.program.erase("TableImporter") # this Reference will free itself
 
 func _import() -> void:
@@ -97,7 +104,7 @@ func _import() -> void:
 			_fields = {}
 			_data_types = []
 			_rows = {}
-			_read_table() # writes wiki_titles; we don't keep data, fields, rows
+			_read_table() # writes wiki_titles; we don't keep anything else
 	for table_name in _table_import:
 		_path = _table_import[table_name]
 		_data = []
@@ -109,6 +116,9 @@ func _import() -> void:
 		# save everything else to Global dicts below
 		_table_data[table_name] = _data
 		_table_fields[table_name] = _fields
+		for i in range(_data_types.size()):
+			if _data_types[i] == "X":
+				_data_types[i] = "BOOL"
 		_table_data_types[table_name] = _data_types
 		_table_units[table_name] = _units
 		_table_row_dicts[table_name] = _rows
@@ -133,7 +143,6 @@ func _read_table() -> void:
 		if commenter != -1 and commenter < 4: # skip comment line
 			line = file.get_line()
 			continue
-		
 		var reg_matches := _split_regex.search_all(line)
 		var n_matches := reg_matches.size()
 		_line_array.resize(n_matches)
@@ -151,18 +160,18 @@ func _read_table() -> void:
 					_fields[field] = n_columns
 					n_columns += 1
 				have_fields = true
-				assert(n_columns == _fields.size(), "Columns: %s, unique fields: %s; duplicate? %s" \
+				assert(n_columns == _fields.size(), "Duplicate field (%s columns, %s unique fields) in %s" \
 						% [n_columns, _fields.size(), _path])
 			elif _line_array[0] == "DataType":
 				_data_types = _line_array.duplicate()
 				_data_types[0] = "STRING" # always name field
 				_data_types.resize(n_columns) # there could be an extra comment column
-				assert(_data_type_test())
+				assert(_data_types_test())
 			elif _line_array[0] == "Units":
 				_units = _line_array.duplicate()
 				_units[0] = ""
 				_units.resize(n_columns)
-				assert(_unit_test())
+				assert(_units_test())
 			elif _line_array[0] == "Default":
 				_defaults = _line_array.duplicate()
 				_defaults[0] = ""
@@ -170,7 +179,10 @@ func _read_table() -> void:
 				i = 0
 				while i < n_columns:
 					if _defaults[i]:
-						_defaults[i] = _get_processed_value(_defaults[i], i)
+						_cell = _defaults[i]
+						_data_type = _data_types[i]
+						_process_cell_value()
+						_defaults[i] = _cell
 					i += 1
 			else:
 				assert(_data_types) # required
@@ -187,11 +199,6 @@ func _read_table() -> void:
 		line = file.get_line()
 
 func _read_data_line() -> void:
-	# We do minimal value modification here:
-	#   - Strip enclosing quotes
-	#   - Strip leanding underscore
-	#   - c_unescape()
-	# But we do some asserts for DataType.
 	var row := _data.size()
 	var row_data := []
 	row_data.resize(_fields.size()) # unfilled row_data are nulls
@@ -199,49 +206,48 @@ func _read_data_line() -> void:
 	assert(!_rows.has(_row_name))
 	_rows[_row_name] = row
 	for field in _fields:
+		_count_cells += 1
 		_field = field
 		var column: int = _fields[_field]
 		_cell = _line_array[column]
-		_cell = _get_processed_value(_cell, column)
+		_data_type = _data_types[column]
+		_process_cell_value()
 		if !_cell: # set to default
 			_cell = _defaults[column]
 		if !_cell:
-			row_data[column] = 0
+			row_data[column] = ""
 			continue
-		assert(_cell_test(column))
-		# value is stored as index for each unique cell value
-		if _value_indexes.has(_cell):
-			row_data[column] = _value_indexes[_cell]
-		else:
-			var index := _values.size()
-			_values.append(_cell)
-			_value_indexes[_cell] = index
-			row_data[column] = index
+		assert(_cell_test())
+		row_data[column] = _cell
+		_unique_register[_cell] = null
 		if _enable_wiki and _field == "wiki_en": # TODO: non-English Wikipedias
 			assert(!_wiki_titles.has(_row_name))
 			_wiki_titles[_row_name] = _cell
-		_count += 1
+		_count_non_null += 1
 	_data.append(row_data)
+	_count_rows += 1
 
-func _get_processed_value(value: String, column: int) -> String:
-	value = value.lstrip("_")
-	value = value.c_unescape() # does not work for "\u"; Godot issue #38716
-	value = StrUtils.c_unescape_patch(value)
-	var data_type: String = _data_types[column]
-	if data_type == "REAL":
-		value = value.replace("E", "e")
-	return value
+func _process_cell_value() -> void:
+	_cell = _cell.lstrip("_")
+	_cell = _cell.c_unescape() # does not work for "\uXXXX"; Godot issue #38716
+	_cell = StrUtils.c_unescape_patch(_cell) # handles "\uXXXX"
+	if _data_type == "BOOL":
+		_cell = _cell.to_upper()
+		if _cell == "FALSE":
+			_cell = ""
+	elif _data_type == "REAL":
+		_cell = _cell.replace("E", "e")
 
-func _data_type_test() -> bool:
+func _data_types_test() -> bool:
 	for data_type in _data_types:
 		assert(DATA_TYPES.has(data_type) or data_type in _enums, "Unknown DataType: " + data_type)
 	return true
 
-func _unit_test() -> bool:
+func _units_test() -> bool:
 	for unit in _units:
 		if unit:
 			assert(unit_defs.is_valid_unit(unit, true, Global.unit_multipliers, Global.unit_functions),
-					"Unkown unit " + unit)
+					"Unkown unit: " + unit)
 	return true
 
 func _table_test(n_columns: int) -> bool:
@@ -264,13 +270,13 @@ func _table_test(n_columns: int) -> bool:
 				assert(data_type in _enums, "Non-existent enum dict " + data_type)
 	return true
 
-func _cell_test(column: int) -> bool:
-	var data_type: String = _data_types[column]
-	match data_type:
+func _cell_test() -> bool:
+	# This is after _process_cell_value()
+	match _data_type:
 		"X":
 			assert(_cell == "x" or _line_error("X type must be x or blank cell"))
 		"BOOL":
-			assert(_cell.matchn("true") or _cell.matchn("false") or _line_error("Expected BOOL"))
+			assert(_cell == "TRUE" or _line_error("Expected BOOL"))
 		"INT":
 			assert(_cell.is_valid_integer() or _line_error("Expected INT"))
 		"REAL":
@@ -278,7 +284,7 @@ func _cell_test(column: int) -> bool:
 		"STRING", "DATA", "BODY":
 			pass
 		_: # must be valid enum name
-			assert(_enums[data_type].has(_cell) or _line_error("Non-existent enum value " + _cell))
+			assert(_enums[_data_type].has(_cell) or _line_error("Non-existent enum value " + _cell))
 	return true
 
 func _line_error(msg := "") -> bool:
