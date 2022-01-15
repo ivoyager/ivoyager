f3173514f25355b2ad1add43c6e93e0570717230
Charlie Whitfield <charliewhitfield@zoho.com>
2022-01-15 09:14:45 -0500

    Improvements to IVOrbitBuilder

--- prog_builders/orbit_builder.gd
+++ prog_builders/orbit_builder.gd
@@ -23,10 +23,12 @@ class_name IVOrbitBuilder
 const math := preload("res://ivoyager/static/math.gd") # =IVMath when issue #37529 fixed
 
 const DPRINT := false
+const PIdiv2 := PI / 2.0
 const MIN_E_FOR_APSIDAL_PRECESSION := 0.0001
 const MIN_I_FOR_NODAL_PRECESSION := deg2rad(0.1)
 
 var _dynamic_orbits: bool = IVGlobal.dynamic_orbits
+var _ecliptic_rotation: Basis = IVGlobal.ecliptic_rotation
 var _table_reader: IVTableReader
 var _Orbit_: Script
 var _d := {
@@ -94,22 +96,30 @@ func make_orbit_from_data(table_name: String, table_row: int, parent: IVBody) ->
 	_table_reader.build_dictionary_from_keys(_d, table_name, table_row)
 
 	# convert to standardized orbital elements [a, e, i, Om, w, M0, n]
-	if is_nan(_d.w):
+	var a : float = _d.a
+	var e : float = _d.e
+	var i : float = _d.i
+	var Om : float = _d.Om
+	var w : float = _d.w
+	var M0 : float = _d.M0
+	var n : float = _d.n
+	
+	if is_nan(w):
 		assert(!is_nan(_d.w_hat))
-		_d.w = _d.w_hat - _d.Om
-	if is_nan(_d.n):
+		w = _d.w_hat - Om
+	if is_nan(n):
 		if !is_nan(_d.L_rate):
-			_d.n = _d.L_rate
+			n = _d.L_rate
 		else:
-			_d.n = sqrt(mu / pow(_d.a, 3))
-	if is_nan(_d.M0):
+			n = sqrt(mu / (a * a * a))
+	if is_nan(M0):
 		if !is_nan(_d.L0):
-			_d.M0 = _d.L0 - _d.w - _d.Om
+			M0 = _d.L0 - w - Om
 		elif !is_nan(_d.T0):
-			_d.M0 = -_d.n * _d.T0
+			M0 = -n * _d.T0
 		else:
 			assert(false, "Elements must include M0, L0 or T0")
-	var elements := [_d.a, _d.e, _d.i, _d.Om, _d.w, _d.M0, _d.n]
+	var elements := [a, e, i, Om, w, M0, n]
 	var orbit: IVOrbit = _Orbit_.new()
 	orbit.elements_at_epoch = elements
 	
@@ -122,13 +132,26 @@ func make_orbit_from_data(table_name: String, table_row: int, parent: IVBody) ->
 		# Rate info (if given) must matches one or the other format.
 		var element_rates: Array # optional
 		var m_modifiers: Array # optional
-		if !is_nan(_d.a_rate): # is planet w/ rates
-			element_rates = [_d.a_rate, _d.e_rate, _d.i_rate, _d.Om_rate, _d.w_rate]
-			assert(!is_nan(_d.e_rate) && !is_nan(_d.i_rate) && !is_nan(_d.Om_rate) && !is_nan(_d.w_rate))
+		
+		var a_rate: float = _d.a_rate
+		var e_rate: float = _d.e_rate
+		var i_rate: float = _d.i_rate
+		var Om_rate: float = _d.Om_rate
+		var w_rate: float = _d.w_rate
+		
+		if !is_nan(a_rate): # is planet w/ rates
+			assert(!is_nan(e_rate) and !is_nan(i_rate) and !is_nan(Om_rate) and !is_nan(w_rate))
+			element_rates = [a_rate, e_rate, i_rate, Om_rate, w_rate]
+			
 			# M modifiers are additional modifiers for Jupiter to Pluto.
-			if !is_nan(_d.M_adj_b): # must also have c, s, f
-				m_modifiers = [_d.M_adj_b, _d.M_adj_c, _d.M_adj_s, _d.M_adj_f]
-				assert(!is_nan(_d.M_adj_c) && !is_nan(_d.M_adj_s) && !is_nan(_d.M_adj_f))
+			var M_adj_b: float = _d.M_adj_b
+			if !is_nan(M_adj_b): # must also have c, s, f
+				var M_adj_c: float = _d.M_adj_c
+				var M_adj_s: float = _d.M_adj_s
+				var M_adj_f: float = _d.M_adj_f
+				assert(!is_nan(M_adj_c) and !is_nan(M_adj_s) and !is_nan(M_adj_f))
+				m_modifiers = [M_adj_b, M_adj_c, M_adj_s, M_adj_f]
+				
 		elif !is_nan(_d.Pw): # moon format
 			assert(!is_nan(_d.Pnode)) # both or neither
 			# Pw, Pnode don'_d tell us the direction of precession! However, I
@@ -137,19 +160,21 @@ func make_orbit_from_data(table_name: String, table_row: int, parent: IVBody) ->
 			# Some values are tiny leading to div/0 or excessive updating. These
 			# correspond to near-circular and/or non-inclined orbits (where Om & w
 			# are technically undefined and updates are irrelevant).
-			if elements[2] < MIN_I_FOR_NODAL_PRECESSION:
+			if i < MIN_I_FOR_NODAL_PRECESSION:
 				_d.Pnode = 0.0
-			if elements[1] < MIN_E_FOR_APSIDAL_PRECESSION:
+			if e < MIN_E_FOR_APSIDAL_PRECESSION:
 				_d.Pw = 0.0
-			var orbit_sign := sign(PI / 2.0 - _d.i) # prograde +1; retrograde -1
-			_d.Om_rate = 0.0
-			_d.w_rate = 0.0
+			var orbit_sign := sign(PIdiv2 - i) # prograde +1; retrograde -1
+			Om_rate = 0.0
+			w_rate = 0.0
 			if _d.Pnode != 0.0:
-				_d.Om_rate = -orbit_sign * TAU / _d.Pnode # opposite to orbit!
+				Om_rate = -orbit_sign * TAU / _d.Pnode # opposite to orbit!
 			if _d.Pw != 0.0:
-				_d.w_rate = orbit_sign * TAU / _d.Pw
-			if _d.Om_rate or _d.w_rate:
-				element_rates = [0.0, 0.0, 0.0, _d.Om_rate, _d.w_rate]
+				w_rate = orbit_sign * TAU / _d.Pw
+			if Om_rate or w_rate:
+				element_rates = [0.0, 0.0, 0.0, Om_rate, w_rate]
+		
+		# add orbit purturbations, if any
 		if element_rates:
 			orbit.element_rates = element_rates
 			if m_modifiers:
@@ -162,10 +187,11 @@ func make_orbit_from_data(table_name: String, table_row: int, parent: IVBody) ->
 		var orbit_ra: float = _table_reader.get_real(table_name, "orbit_RA", table_row)
 		var orbit_dec: float = _table_reader.get_real(table_name, "orbit_dec", table_row)
 		orbit.reference_normal = math.convert_spherical2(orbit_ra, orbit_dec)
-		orbit.reference_normal = IVGlobal.ecliptic_rotation * orbit.reference_normal
+		orbit.reference_normal = _ecliptic_rotation * orbit.reference_normal
 		orbit.reference_normal = orbit.reference_normal.normalized()
 	elif _d.ref_plane:
 		assert(_d.ref_plane == "Ecliptic")
+		
 	# reset for next orbit build
 	_reset_table_dict()
 	return orbit
