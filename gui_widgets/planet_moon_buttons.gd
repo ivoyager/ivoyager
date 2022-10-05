6353ce08158839eab4568863e0df8642001b2178
Charlie Whitfield <charliewhitfield@zoho.com>
2022-10-05 17:48:24 -0400

    New IVNavigationButton replaces previous inner class

--- gui_widgets/planet_moon_buttons.gd
+++ gui_widgets/planet_moon_buttons.gd
@@ -136,7 +136,7 @@ func _clear() -> void:
 
 
 func _add_nav_button(box_container: BoxContainer, body: IVBody, image_size: float) -> void:
-	var button := NavButton.new(body, image_size, _selection_manager)
+	var button := IVNavigationButton.new(body, image_size, _selection_manager)
 	button.connect("selected", self, "_on_nav_button_selected", [button])
 	button.size_flags_horizontal = SIZE_FILL
 	box_container.add_child(button)
@@ -193,63 +193,3 @@ func _settings_resize() -> void:
 	for child in get_children():
 		child.show()
 
-
-# ****************************** INNER CLASS **********************************
-
-class NavButton extends Button:
-	
-	signal selected()
-	
-	var _selection_manager: IVSelectionManager
-	var _body: IVBody # this button
-	var _has_mouse := false
-	
-	
-	func _init(body: IVBody, image_size: float, selection_manager: IVSelectionManager) -> void:
-		_body = body
-		_selection_manager = selection_manager
-		toggle_mode = true
-		hint_tooltip = body.name
-		set("custom_fonts/font", IVGlobal.fonts.two_pt) # hack to allow smaller button height
-		rect_min_size = Vector2(image_size, image_size)
-		flat = true
-		focus_mode = FOCUS_ALL
-		var texture_box := TextureRect.new()
-		texture_box.set_anchors_and_margins_preset(PRESET_WIDE, PRESET_MODE_KEEP_SIZE, 0)
-		texture_box.expand = true
-		texture_box.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
-		texture_box.texture = body.texture_2d
-		texture_box.mouse_filter = MOUSE_FILTER_IGNORE
-		add_child(texture_box)
-		connect("mouse_entered", self, "_on_mouse_entered")
-		connect("mouse_exited", self, "_on_mouse_exited")
-
-
-	func _ready():
-		IVGlobal.connect("update_gui_requested", self, "_update_selection")
-		_selection_manager.connect("selection_changed", self, "_update_selection")
-		_selection_manager.connect("selection_reselected", self, "_update_selection")
-		action_mode = BaseButton.ACTION_MODE_BUTTON_PRESS
-		set_default_cursor_shape(CURSOR_POINTING_HAND)
-
-
-	func _pressed() -> void:
-		_selection_manager.select_body(_body)
-
-
-	func _update_selection() -> void:
-		var is_selected := _selection_manager.get_body() == _body
-		pressed = is_selected
-		if is_selected:
-			emit_signal("selected")
-		flat = !is_selected and !_has_mouse
-
-
-	func _on_mouse_entered() -> void:
-		_has_mouse = true
-		flat = false
-
-
-	func _on_mouse_exited() -> void:
-		_has_mouse = false
-		flat = !pressed
