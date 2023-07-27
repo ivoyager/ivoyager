68d717030e5887d051d5fd47bcf68d117e77fc1c
Charlie Whitfield <charliewhitfield@zoho.com>
2023-07-27 15:17:57 -0400

    Fix static/files.gd for 4.x API changes [Needs testing!]

--- static/files.gd
+++ static/files.gd
@@ -49,8 +49,8 @@ static func get_save_dir_path(is_modded: bool, override_dir: String = "") -> Str
 			if !save_dir.ends_with("/unmodded_saves"):
 				save_dir = ""
 	if save_dir:
-		var dir := DirAccess.new()
-		if dir.open(save_dir) != OK:
+		var dir := DirAccess.open(save_dir)
+		if !dir:
 			save_dir = ""
 	if save_dir == "":
 		save_dir = OS.get_user_data_dir() + "/saves"
@@ -72,7 +72,7 @@ static func get_base_file_name(file_name : String) -> String:
 
 
 static func get_save_path(save_dir: String, base_name: String, date_string := "", append_file_extension := false) -> String:
-	var path := save_dir.plus_file(base_name)
+	var path := save_dir.path_join(base_name)
 	if date_string:
 		path += "." + date_string
 	if append_file_extension:
@@ -81,61 +81,83 @@ static func get_save_path(save_dir: String, base_name: String, date_string := ""
 
 
 static func exists(file_path: String) -> bool:
-	var file := File.new()
-	if file_path.ends_with(".gd"):
-		# Godot exported has ".gd" changed to ".gdc"
-		return file.file_exists(file_path) or file.file_exists(file_path + "c")
-	return file.file_exists(file_path)
+	# TEST34: If works, remove this func and use directly.
+	return ResourceLoader.exists(file_path)
+#	var file := File.new()
+#	if file_path.ends_with(".gd"):
+#		# Godot exported has ".gd" changed to ".gdc"
+#		return file.file_exists(file_path) or file.file_exists(file_path + "c")
+#	return file.file_exists(file_path)
 
 
 static func is_valid_dir(dir_path: String) -> bool:
-	if dir_path == "":
-		return false
-	var dir := DirAccess.new()
-	return dir.open(dir_path) == OK
+	# TEST34: If works, remove this func and use directly.
+	return DirAccess.dir_exists_absolute(dir_path)
+#	if dir_path == "":
+#		return false
+#	var dir := DirAccess.new()
+#	return dir.open(dir_path) == OK
 
 
-static func make_dir_if_doesnt_exist(dir_path: String, recursive := true) -> void:
-	var dir := DirAccess.new()
-	if !dir.dir_exists(dir_path):
-		if recursive:
-			dir.make_dir_recursive(dir_path)
-		else:
-			dir.make_dir(dir_path)
+static func make_dir_if_doesnt_exist(dir_path: String) -> void:
+	# TEST34: Do we need exists test? If not, remove this func and use directly.
+	if DirAccess.dir_exists_absolute(dir_path):
+		return
+	DirAccess.make_dir_recursive_absolute(dir_path)
+#	var dir := DirAccess.new()
+#	if !dir.dir_exists(dir_path):
+#		if recursive:
+#			dir.make_dir_recursive(dir_path)
+#		else:
+#			dir.make_dir(dir_path)
 
 
 static func make_or_clear_dir(dir_path: String) -> void:
-	var dir := DirAccess.new()
-	if dir.dir_exists(dir_path):
-		assert(dir.open(dir_path) == OK)
-		dir.list_dir_begin() # TODOConverter3To4 fill missing arguments https://github.com/godotengine/godot/pull/40547
-		var file_name := dir.get_next()
-		while file_name:
-			if !dir.current_is_dir():
-				dir.remove(file_name)
-			file_name = dir.get_next()
-	else:
-		dir.make_dir(dir_path)
-
-
-# loading assets & data files
-
-static func get_dir_files(dir_path: String, extension := "") -> Array:
-	# No period in extension, if provided.
-	# Useful for debugging. Export removes files & changes file names!
-	var dir := DirAccess.new()
-	if dir.open(dir_path) != OK:
-		print("Could not open dir: ", dir_path)
-		return []
-	var result := []
+	# TEST34
+	if !DirAccess.dir_exists_absolute(dir_path):
+		DirAccess.make_dir_recursive_absolute(dir_path)
+		return
+	var dir := DirAccess.open(dir_path)
+	dir.include_hidden = false
+	dir.include_navigational = false
 	dir.list_dir_begin() # TODOConverter3To4 fill missing arguments https://github.com/godotengine/godot/pull/40547
 	var file_name := dir.get_next()
 	while file_name:
 		if !dir.current_is_dir():
-			if !extension or file_name.get_extension() == extension:
-				result.append(file_name)
+			dir.remove(file_name)
 		file_name = dir.get_next()
-	return result
+#	var dir := DirAccess.new()
+#	if dir.dir_exists(dir_path):
+#		assert(dir.open(dir_path) == OK)
+#		dir.list_dir_begin() # TODOConverter3To4 fill missing arguments https://github.com/godotengine/godot/pull/40547
+#		var file_name := dir.get_next()
+#		while file_name:
+#			if !dir.current_is_dir():
+#				dir.remove(file_name)
+#			file_name = dir.get_next()
+#	else:
+#		dir.make_dir(dir_path)
+
+
+# loading assets & data files
+
+# TODO34: Update debug func below for 4.x
+#static func get_dir_files(dir_path: String, extension := "") -> Array:
+#	# No period in extension, if provided.
+#	# Useful for debugging. Export removes files & changes file names!
+#	var dir := DirAccess.new()
+#	if dir.open(dir_path) != OK:
+#		print("Could not open dir: ", dir_path)
+#		return []
+#	var result := []
+#	dir.list_dir_begin() # TODOConverter3To4 fill missing arguments https://github.com/godotengine/godot/pull/40547
+#	var file_name := dir.get_next()
+#	while file_name:
+#		if !dir.current_is_dir():
+#			if !extension or file_name.get_extension() == extension:
+#				result.append(file_name)
+#		file_name = dir.get_next()
+#	return result
 
 
 static func find_resource_file(dir_paths: Array, prefix: String,
@@ -145,12 +167,18 @@ static func find_resource_file(dir_paths: Array, prefix: String,
 	# find file with .import extension (this is the ONLY file in an exported
 	# project!), but ".import" must be removed from end to load it.
 	# Search is case-insensitive.
+	
+	# TEST34: Do *.import files still exist in the file system?
+	
 	var prefix_dot := prefix + "."
 	var match_size := prefix_dot.length()
-	var dir := DirAccess.new()
+	var dir: DirAccess
 	for dir_path in dir_paths:
-		if dir.open(dir_path) != OK:
+		dir = DirAccess.open(dir_path)
+		if !dir:
 			continue
+		dir.include_hidden = false
+		dir.include_navigational = false
 		dir.list_dir_begin() # TODOConverter3To4 fill missing arguments https://github.com/godotengine/godot/pull/40547
 		var file_name := dir.get_next()
 		while file_name:
@@ -166,6 +194,29 @@ static func find_resource_file(dir_paths: Array, prefix: String,
 						return subdir_result
 			file_name = dir.get_next()
 	return ""
+
+
+#	var prefix_dot := prefix + "."
+#	var match_size := prefix_dot.length()
+#	var dir := DirAccess.new()
+#	for dir_path in dir_paths:
+#		if dir.open(dir_path) != OK:
+#			continue
+#		dir.list_dir_begin() # TODOConverter3To4 fill missing arguments https://github.com/godotengine/godot/pull/40547
+#		var file_name := dir.get_next()
+#		while file_name:
+#			if !dir.current_is_dir():
+#				if file_name.get_extension() == "import":
+#					if file_name.substr(0, match_size).matchn(prefix_dot):
+#						return dir_path.plus_file(file_name).get_basename()
+#			elif search_prefix_subdirectories:
+#				if file_name.matchn(prefix):
+#					var subdir_path: String = dir_path + "/" + file_name
+#					var subdir_result := find_resource_file([subdir_path], prefix, false)
+#					if subdir_result:
+#						return subdir_result
+#			file_name = dir.get_next()
+#	return ""
 
 
 static func find_and_load_resource(dir_paths: Array, prefix: String,
