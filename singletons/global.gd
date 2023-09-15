3b51650c9581976f557dd0e11a6a6f64184c0f54
Charlie Whitfield <charliewhitfield@zoho.com>
2023-09-14 21:59:03 -0400

    Update ymd

--- singletons/global.gd
+++ singletons/global.gd
@@ -30,7 +30,7 @@ extends Node
 const IVOYAGER_VERSION := "0.0.16"
 const IVOYAGER_BUILD := "" # hotfix or debug build
 const IVOYAGER_STATE := "dev" # 'dev', 'alpha', 'beta', 'rc', ''
-const IVOYAGER_YMD := 20230909
+const IVOYAGER_YMD := 20230914
 
 
 # simulator state broadcasts
